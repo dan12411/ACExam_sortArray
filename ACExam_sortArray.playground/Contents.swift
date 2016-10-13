25bd59f90435ad9033053f65bc1f3757278383e6
TeChengHung <dan12411@gmail.com>
2016-10-13 16:52:51 +0800

    Modify my answer(no use Set) & Add Eric's solution

--- ACExam_sortArray.playground/Contents.swift
+++ ACExam_sortArray.playground/Contents.swift
@@ -3,38 +3,61 @@ import UIKit
 
 func sortArray(array1: [Int], array2: [Int]) -> [Int] {
     
-    var arrayOne = array1
-    var arrayTwo = array2
-    var resultArray = [0,1]
-    
-    for i in 0..<arrayOne.count {
-        for j in 0..<arrayTwo.count {
-            if arrayOne[i] == arrayTwo[j] {
-                arrayOne.remove(at: i)
-            }
+    var array3 = array1 + array2
+    var arrayDict = [Int:Int]()
+    for n in array3 {
+        if arrayDict[n] == nil {
+            arrayDict[n] = 1
+        } else {
+            arrayDict[n] = arrayDict[n]! + 1
         }
     }
     
-    resultArray = array2
+//    arrayDict
+//    
+//    for i in arrayDict.keys {
+//        if arrayDict[i]! >= 2 {
+//            arrayDict.removeValue(forKey: i)
+//        }
+//    }
     
-    for i in 0..<arrayOne.count {
-        resultArray.append(arrayOne[i])
-    }
+    array3 = Array(arrayDict.keys)
+
     
-    for i in 0..<resultArray.count-2 {
-        for j in 0..<resultArray.count - 2 - i {
+    for i in 0 ..< array3.count-2 {
+        for j in 0 ..< array3.count - 2 - i {
             var tmp = 0
-            if resultArray[j] > resultArray [j + 1] {
-                tmp = resultArray[j + 1]
-                resultArray[j + 1] = resultArray[j]
-                resultArray[j] = tmp
+            if array3[j] > array3[j + 1] {
+                tmp = array3[j + 1]
+                array3[j + 1] = array3[j]
+                array3[j] = tmp
             }
         }
     }
 
-    return resultArray
+    return array3
 }
 
-sortArray(array1: [1,5,3], array2: [2,4,3])
+sortArray(array1: [1,5,3,1], array2: [2,4,3])
+
+
+/// Eric's solution ///
+func sortArray2(array1: [Int], array2: [Int]) -> [Int] {
+    var array3 = array1 + array2
+    array3 = Array(Set(array3))
+    for i in 0..<array3.count - 1 {
+        for j in 0..<array3.count - 1 {
+            if (array3[i] > array3[j + 1] && i < j + 1) {
+                swap(&array3[i], &array3[j + 1])
+            }
+        }
+    }
+    return array3
+}
+
+// 集合(Set)用來儲存相同型別且沒有順序、沒有重複的值，當順序不重要或是需要每個值只能出現一次時，可以選擇使用Set
+
+sortArray2(array1: [2,5,1,2,99,3,7,33,222], array2: [1,1,111,999,234,23,44,11])
+sortArray(array1: [2,5,1,2,99,3,7,33,222], array2: [1,1,111,999,234,23,44,11])
 
 
